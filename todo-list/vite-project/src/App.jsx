a87809b34d0c4270a0a4cdb6e7a5cdc536b7400e
Logan Lee <leelogan93@gmail.com>
2023-09-14 23:19:48 -0700

    Complete todo list.

---
+++ todo-list/vite-project/src/App.jsx
@@ -0,0 +1,65 @@
+import { TodoItem } from './TodoItem';
+import './styles.css';
+import { useState } from 'react';
+
+function App() {
+	const [newTodoName, setNewTodoName] = useState('');
+	const [todos, setTodos] = useState([]);
+	console.log(newTodoName);
+
+	function addNewTodo() {
+		if (newTodoName === '') return;
+		setTodos((currentTodos) => {
+			return [
+				...currentTodos,
+				{ name: newTodoName, completed: false, id: crypto.randomUUID() },
+			];
+		});
+		setNewTodoName('');
+	}
+
+	function toggleTodo(todoId, completed) {
+		setTodos((currentTodos) => {
+			return currentTodos.map((todo) => {
+				if (todo.id === todoId) return { ...todo, completed };
+				return todo;
+			});
+		});
+	}
+
+	function deleteTodo(todoId) {
+		setTodos((currentTodos) => {
+			return currentTodos.filter((todo) => todo.id !== todoId);
+		});
+	}
+
+	return (
+		<>
+			<ul id="list">
+				{todos.map((todo) => {
+					return (
+						<TodoItem
+							key={todo.id}
+							{...todo}
+							toggleTodo={toggleTodo}
+							deleteTodo={deleteTodo}
+						/>
+					);
+				})}
+			</ul>
+
+			<div id="new-todo-form">
+				<label htmlFor="todo-input">New Todo</label>
+				<input
+					type="text"
+					id="todo-input"
+					value={newTodoName}
+					onChange={(e) => setNewTodoName(e.target.value)}
+				/>
+				<button onClick={addNewTodo}>Add Todo</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
